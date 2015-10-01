1836bda8a3cd3d9f2b16466239f7b95db476666f
Pierre Laperdrix <pierre.laperdrix@inria.fr>
2015-10-01 17:27:56 +0200

    Fixed long waiting time on startup

--- data/page-script.js
+++ data/page-script.js
@@ -11,11 +11,15 @@ function clearIframe() {
     self.port.emit("getNbEvol");
 }
 
+function sendFP(){
+    loadIframe();
+    setTimeout(clearIframe,10000);
+}
+
+//Send FP on startup
+sendFP();
+
 //Send the FP every 4 hours to the server
-setInterval(
-    function(){
-        loadIframe();
-        setTimeout(clearIframe,10000);
-    },
+setInterval(sendFP,
     4*60*60*1000
 );
